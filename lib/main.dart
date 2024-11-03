59f447689645d7e44281ba3b321f848b43ade59b
Daulet <daulet.tusupbaev@gmail.com>
2024-11-03 08:30:42 +0500

    feat: logging bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_guess_number_app/features/app/app.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
 
 void main() {
+  Bloc.observer = TalkerBlocObserver();
   runApp(const App());
 }
